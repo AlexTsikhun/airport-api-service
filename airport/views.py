fa4fd3f8c8934ab5a47173ae3be152a852fd496f
Alexander <sasacihun@gmail.com>
2024-06-04 13:25:32 +0300

    feat: Baseline (Serializer->View->Url) for Airplane

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import mixins
+from airport.models import (
+    AirplaneType
+)
+from airport.serializers import (
+    AirplaneTypeSerializer
+)
 
-# Create your views here.
+
+class AirplaneTypeViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
