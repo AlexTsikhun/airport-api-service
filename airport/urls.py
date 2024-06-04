93c00d2e8c5c8095128015b98645f215cead134b
Alexander <sasacihun@gmail.com>
2024-06-04 17:34:31 +0300

    fixup! refactor: Add plural form (s) for router names

--- airport/urls.py
+++ airport/urls.py
@@ -14,13 +14,6 @@ from airport.views import (
 app_name = "airport"
 
 router = routers.DefaultRouter()
-router.register("airplane_type", AirplaneTypeViewSet)
-router.register("airport", AirportViewSet)
-router.register("crew", CrewViewSet)
-router.register("order", OrderViewSet)
-router.register("airplane", AirplaneViewSet)
-router.register("route", RouteViewSet)
-router.register("flight", FlightViewSet)
 router.register("airplane_types", AirplaneTypeViewSet)
 router.register("airports", AirportViewSet)
 router.register("crews", CrewViewSet)
